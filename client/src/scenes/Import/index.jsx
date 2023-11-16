fd473358c877f4a2392053b3f8f351d95b6280c4
Clyde <jcayap18@gmail.com>
2023-11-16 18:11:08 +0800

    patch

--- client/src/scenes/Import/index.jsx
+++ client/src/scenes/Import/index.jsx
@@ -9,7 +9,7 @@ function formatDateToYYYYMMDD(excelDate) {
   const year = date.getFullYear();
   const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Adding 1 because months are zero-based
   const day = date.getDate().toString().padStart(2, '0');
-  return `${month}-${day}-${year}`;
+  return `${year}-${month}-${day}`;
 }
 
 // const formatDateToYYYYMMDD = (excelDate) => {
