23aa92d2d011f44a7cdba11d0e9fe6dfc4555d12
Clyde <jcayap18@gmail.com>
2023-11-16 18:08:12 +0800

    poblacion

--- client/src/scenes/Import/index.jsx
+++ client/src/scenes/Import/index.jsx
@@ -9,7 +9,7 @@ function formatDateToYYYYMMDD(excelDate) {
   const year = date.getFullYear();
   const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Adding 1 because months are zero-based
   const day = date.getDate().toString().padStart(2, '0');
-  return `${year}-${month}-${day}`;
+  return `${month}-${day}-${year}`;
 }
 
 // const formatDateToYYYYMMDD = (excelDate) => {
